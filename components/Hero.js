fb5884a6b334d32b86a0b33c9860313a1e0b8f07
David Porosnicu <david.porosnicu@e-spres-oh.com>
2019-10-16 17:33:36 +0300

    add hero for alpine consolidated

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
 import styles from "./Hero.modules.css";
+import Button from "./Button";
 
 class Hero extends Component {
   render() {
     return (
-      <div>
-        <h1 className={styles.specialTitle}>
-          WASHINGTON BASED VENTURE INVESTMENT COMPANY SINCE 1996
-        </h1>
-        <p>
-          An unparalleled history of successful investor returns by
-          capitalizing, growing, and exiting unique business models in
-          industries few could envision.
-        </p>
+      <div className="column-grid">
+        <div className="box-half">
+          <div className={styles.textBox}>
+            <h1 className={styles.specialTitle}>
+              <span style={{ whiteSpace: "nowrap" }}>WASHINGTON BASED</span>{" "}
+              <br />
+              <span
+                style={{ whiteSpace: "nowrap" }}
+                className={styles.highlight}
+              >
+                VENTURE INVESTMENT
+              </span>{" "}
+              <br />
+              <span style={{ whiteSpace: "nowrap" }}>COMPANY SINCE 1996</span>
+            </h1>
+            <p className={styles.text}>
+              An unparalleled history of successful investor returns by
+              capitalizing, growing, and exiting unique business models in
+              industries few could envision.
+            </p>
+
+            <div className={styles.buttons}>
+              <Button text="CONTACT US" contact />
+              <Button text="LEARN MORE" />
+            </div>
+          </div>
+        </div>
+        <div className="box-half">
+          {/* <img
+            className={`${styles.backgroundImage}`}
+            src={backgroundImage}
+          ></img> */}
+        </div>
       </div>
     );
   }
