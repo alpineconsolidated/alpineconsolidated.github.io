14805ed4c9d86538e5a981c1744ad607c6b41974
David Porosnicu <david.porosnicu@e-spres-oh.com>
2019-10-18 10:40:30 +0200

    add 'OUR FOCUS' section

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 import Page from "../../layouts/Page";
 import Hero from "../../components/Hero";
 import Parallax from "../../components/Parallax";
+import PersonCard from "../../components/PersonCard";
 
 import styles from "./index.modules.css";
 
@@ -11,6 +12,37 @@ const Index = props => (
   <Page>
     <section className={`${styles.hero} black`}>
       <Hero />
+    </section>
+    <section className={`white`}>
+      <div className="column-grid">
+        <div className="hideOnSmallTablet box-half">
+          <Parallax image={triangle1} />
+        </div>
+        <div className={`${styles.textContainer} box-half`}>
+          <div>
+            <h1>
+              <span className="noWrap">OUR FOCUS</span>
+            </h1>
+            <h3>
+              We invest in entrepreneurs who we can help make waves developing
+              new services across multiple sectors of today’s economy.
+            </h3>
+
+            <div className="showOnSmallTablet">
+              <Parallax image={triangle1} />
+            </div>
+
+            <p>
+              Since 1996, Alpine Consolidated and its team have structured, led
+              and delivered multiple initial public offerings and exits in the
+              US and the UK delivering outsized returns to investors and
+              successful exits every time. It has created new industry leaders
+              in fragmented industries by backing disciplined and practical
+              innovators that have redefined business for the 21st century.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   </Page>
 );
