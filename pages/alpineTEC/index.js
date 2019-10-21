a47047a0018a462e2e258d1a536b6590b6d986fd
David Porosnicu <david.porosnicu@e-spres-oh.com>
2019-10-21 20:30:51 +0200

    add 'what we're looking for' section

--- pages/alpineTEC/index.js
+++ pages/alpineTEC/index.js
@@ -6,6 +6,8 @@ import ColumnBox from "../../components/ColumnBox";
 import Map from "../../components/Map";
 import styles from "./index.modules.css";
 
+import triangle5 from "../../static/images/triangle5.svg";
+
 const AlpineTEC = () => (
   <Page>
     <section className={`${styles.hero} black`}>
@@ -22,6 +24,46 @@ const AlpineTEC = () => (
 
     <section className="black">
       <ColumnBox />
+    </section>
+
+    <section className={`white`}>
+      <div className="column-grid">
+        <div className="hideOnSmallTablet box-half">
+          <div className={styles.imageContainer}>
+            <div className={styles.triangleImage}>
+              <img className="responsive-image" src={triangle5} />
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.textContainer} box-half`}>
+          <div>
+            <h1>
+              <span className="noWrap">WHAT WE'RE</span>
+              <br />
+              <span className="noWrap">LOOKING FOR</span>
+            </h1>
+            <h3>
+              We are most interested in SaaS companies and tech enabled services
+              business or digital transformation opportunities in more
+              traditional situations.
+            </h3>
+
+            <div className="showOnSmallTablet">
+              <div className={styles.imageContainer}>
+                <div className={styles.triangleImage}>
+                  <img className="responsive-image" src={triangle5} />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              We are most interested in SaaS companies and tech enabled services
+              business or digital transformation opportunities in more
+              traditional situations.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   </Page>
 );
