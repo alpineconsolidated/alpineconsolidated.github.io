06205d5c4149278f6ee1b44572a02052c3ee4a76
David Porosnicu <david.porosnicu@e-spres-oh.com>
2019-10-25 12:28:42 +0300

    test

--- pages/alpineTEC/index.js
+++ pages/alpineTEC/index.js
@@ -10,70 +10,77 @@ import styles from "./index.modules.css";
 import triangle5 from "../../static/images/triangle5.svg";
 import logoAlpineTEC from "../../static/images/AlpineTEC_V.svg";
 
-const AlpineTEC = () => (
-  <Page>
-    <section className={`${styles.hero} black`}>
-      <Hero />
-    </section>
+class AlpineTEC extends React.Component {
+  static getInitialProps(ctx) {
+    return {};
+  }
+  render() {
+    return (
+      <Page>
+        <section className={`${styles.hero} black`}>
+          <Hero />
+        </section>
 
-    <section className="whiteTEC">
-      <Testimonial />
-    </section>
+        <section className="whiteTEC">
+          <Testimonial />
+        </section>
 
-    <section className="whiteTEC">
-      <LetterBox />
-    </section>
+        <section className="whiteTEC">
+          <LetterBox />
+        </section>
 
-    <section className="black">
-      <ColumnBox />
-    </section>
+        <section className="black">
+          <ColumnBox />
+        </section>
 
-    <section className={`white`}>
-      <div className="column-grid">
-        <div className="hideOnSmallTablet box-half">
-          <div className={styles.imageContainer}>
-            <div className={styles.triangleImage}>
-              <img className="responsive-image" src={triangle5} />
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.textContainer} box-half`}>
-          <div>
-            <h1>
-              <span className="noWrap">WHAT WE'RE</span>
-              <br />
-              <span className="noWrap">LOOKING FOR</span>
-            </h1>
-            <h3>
-              We are most interested in SaaS companies and tech enabled services
-              business or digital transformation opportunities in more
-              traditional situations.
-            </h3>
-
-            <div className="showOnSmallTablet">
+        <section className={`white`}>
+          <div className="column-grid">
+            <div className="hideOnSmallTablet box-half">
               <div className={styles.imageContainer}>
                 <div className={styles.triangleImage}>
                   <img className="responsive-image" src={triangle5} />
                 </div>
               </div>
             </div>
+            <div className={`${styles.textContainer} box-half`}>
+              <div>
+                <h1>
+                  <span className="noWrap">WHAT WE'RE</span>
+                  <br />
+                  <span className="noWrap">LOOKING FOR</span>
+                </h1>
+                <h3>
+                  We are most interested in SaaS companies and tech enabled
+                  services business or digital transformation opportunities in
+                  more traditional situations.
+                </h3>
 
-            <p>
-              We are most interested in SaaS companies and tech enabled services
-              business or digital transformation opportunities in more
-              traditional situations.
-            </p>
+                <div className="showOnSmallTablet">
+                  <div className={styles.imageContainer}>
+                    <div className={styles.triangleImage}>
+                      <img className="responsive-image" src={triangle5} />
+                    </div>
+                  </div>
+                </div>
+
+                <p>
+                  We are most interested in SaaS companies and tech enabled
+                  services business or digital transformation opportunities in
+                  more traditional situations.
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
-    <section className="white">
-      <Map />
-    </section>
-    <section className="contactSection">
-      <ContactForm logo={logoAlpineTEC} />
-    </section>
-  </Page>
-);
+        </section>
+        <section className="white">
+          <Map />
+        </section>
+        <section className="contactSection">
+          <ContactForm logo={logoAlpineTEC} />
+        </section>
+      </Page>
+    );
+  }
+}
 
 export default AlpineTEC;
