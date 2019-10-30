6cd94851bc04453db19fe929feba0aaa28d623de
David Porosnicu <david.porosnicu@e-spres-oh.com>
2019-10-30 21:23:46 +0200

    add reverse advisor modal on hover functionality

--- components/Advisors.js
+++ components/Advisors.js
@@ -1,5 +1,8 @@
+import ReactDOM from "react-dom";
+
 import styles from "./Advisors.modules.css";
 import greenDownArrow from "../static/images/greenDownArrow.svg";
+import blackSmallTriangle from "../static/images/blackSmallTriangle.svg";
 
 const advisors = [
   {
@@ -150,9 +153,27 @@ const advisors = [
 ];
 
 class Advisors extends React.Component {
-  state = {
-    advisors: advisors
-  };
+  constructor(props) {
+    super(props);
+    this.state = { advisors: advisors };
+    this.myRef = [];
+  }
+
+  componentDidMount() {
+    const advisors = this.state.advisors.map(advisor => {
+      if (
+        this.myRef[advisor.name].getBoundingClientRect().x >
+        window.innerWidth / 2
+      ) {
+        return {
+          name: advisor.name,
+          info: advisor.info,
+          isOffset: true
+        };
+      } else return advisor;
+    });
+    this.setState({ advisors });
+  }
 
   showAdvisorModal = e => {
     const target = this.state.advisors.filter(
@@ -182,36 +203,68 @@ class Advisors extends React.Component {
           <h1>ADVISORS</h1>
         </div>
         <div className={styles.peopleContainer}>
-          {this.state.advisors.map(person => (
-            <div
-              key={person.name}
-              onClick={this.showAdvisorModal}
-              id={person.name}
-              className={`${person.showInfo ? styles.black : ""} ${
-                styles.personContainer
-              }`}
-            >
-              <div className={styles.person}>
+          {this.state.advisors.map(person => {
+            return (
+              <div
+                key={person.name}
+                ref={person.name}
+                onClick={this.showAdvisorModal}
+                id={person.name}
+                ref={ref => {
+                  this.myRef[person.name] = ref;
+                  return true;
+                }}
+                className={`${person.showInfo ? styles.black : ""} ${
+                  styles.personContainer
+                }`}
+              >
+                <div className={styles.person}>
+                  <div
+                    className={
+                      person.showInfo
+                        ? styles.greenUpArrow
+                        : styles.greenDownArrow
+                    }
+                  >
+                    <img src={greenDownArrow} />
+                  </div>
+                  <div className={styles.name}>
+                    <h5>{person.name}</h5>
+                  </div>
+                </div>
                 <div
                   className={
-                    person.showInfo
-                      ? styles.greenUpArrow
-                      : styles.greenDownArrow
+                    person.isOffset
+                      ? styles.reversePersonModal
+                      : styles.personModal
                   }
                 >
-                  <img src={greenDownArrow} />
+                  <img
+                    className={
+                      person.isOffset
+                        ? styles.reverseBlackTriangle
+                        : styles.blackTriangle
+                    }
+                    src={blackSmallTriangle}
+                  />
+                  <h4 className={styles.personModal_name}>{person.name}</h4>
+                  <p>
+                    President and COO of the Salt Lake Organizing Committee for
+                    the 2002 Olympic Winter Games; Managing Director, Sorenson
+                    Capital; Former Chairman and Board Member,
+                    Omniture,(NASDAQ:OMTR). Co-founder Alpine Consolidated, LLC.
+                    Co-founder Travel Services International (NASDAQ:TRVL), and
+                    Resortquest International (NYSE:RQI).
+                  </p>
                 </div>
-                <div className={styles.name}>
-                  <h5>{person.name}</h5>
-                </div>
+                {person.showInfo ? (
+                  <p className={styles.info}>{person.info}</p>
+                ) : (
+                  ""
+                )}
               </div>
-              {person.showInfo ? (
-                <p className={styles.info}>{person.info}</p>
-              ) : (
-                ""
-              )}
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     );
