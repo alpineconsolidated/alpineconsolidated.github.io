c2e046270f69a866e03630b096700e3085cfaf8d
Marcel Barbulescu <marcelbarbulescu@gmail.com>
2019-10-31 17:41:33 -0400

    update email to info@alpineconsolidated.com

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -5,12 +5,12 @@ export default async function sendEmail(req, res) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
-    to: "y3times@gmail.com",
-    from: "no-reply@e-spres-oh.com",
-    subject: "New mail from contact form on -- www.placeholder.com --",
-    text: `You are receiving this email because somebody sent a message through the contact form on -- www.placeholder.com --
+    to: "info@alpineconsolidated.com",
+    from: "no-reply@alpineconsolidated.com",
+    subject: "New mail from contact form on -- www.alpineconsolidated.com --",
+    text: `You are receiving this email because somebody sent a message through the contact form on -- www.alpineconsolidated.com --
       Name: ${formData.name} Phone:${formData.phone} Email: ${formData.email} Message: ${formData.message}`,
-    html: `<p>You are receiving this email because somebody sent a message through the contact form on -- www.placeholder.com --</p>
+    html: `<p>You are receiving this email because somebody sent a message through the contact form on -- www.alpineconsolidate.com --</p>
       <h3>Name: ${formData.name}</h3><p>Phone: ${formData.phone}</p><p>Email: ${formData.email}</p><p>Message: ${formData.message}</p>`
   };
 
