92a40e6679e98a591c4af8afcb13ed49b8086e13
David Porosnicu <david.porosnicu@e-spres-oh.com>
2019-11-06 16:13:37 +0200

    hero changes

--- components/Hero.js
+++ components/Hero.js
@@ -7,25 +7,26 @@ class Hero extends Component {
     return (
       <div className="column-grid">
         <div className="box-half">
-          <div className={styles.textBox}>
-            <h1 className={styles.specialTitle}>
-              <span style={{ whiteSpace: "nowrap" }}>WASHINGTON BASED</span>{" "}
-              <br />
-              <span
-                style={{ whiteSpace: "nowrap" }}
-                className={styles.highlight}
-              >
-                VENTURE INVESTMENT
-              </span>{" "}
-              <br />
-              <span style={{ whiteSpace: "nowrap" }}>COMPANY SINCE 1996</span>
-            </h1>
-            <h4 className={styles.text}>
-              An unparalleled history of successful investor returns by
-              capitalizing, growing, and exiting unique business models in
-              industries few could envision.
-            </h4>
-
+          <div className={styles.container}>
+            <div className={styles.textBox}>
+              <h1 className={styles.specialTitle}>
+                <span style={{ whiteSpace: "nowrap" }}>WASHINGTON BASED</span>{" "}
+                <br />
+                <span
+                  style={{ whiteSpace: "nowrap" }}
+                  className={styles.highlight}
+                >
+                  VENTURE INVESTMENT
+                </span>{" "}
+                <br />
+                <span style={{ whiteSpace: "nowrap" }}>COMPANY SINCE 1996</span>
+              </h1>
+              <h4 className={styles.text}>
+                An unparalleled history of successful investor returns by
+                capitalizing, growing, and exiting unique business models in
+                industries few could envision.
+              </h4>
+            </div>
             <div className={styles.buttons}>
               <a href="#contact">
                 <button className={`${styles.btnFull} ${styles.btn}`}>
--- components/HeroVentures.js
+++ components/HeroVentures.js
@@ -6,12 +6,15 @@ class Hero extends Component {
     return (
       <div className="column-grid">
         <div className="box-half">
-          <div className={styles.textBox}>
-            <h1>
-              <span style={{ whiteSpace: "nowrap" }}>ACCELERATING</span> <br />
-              <span style={{ whiteSpace: "nowrap" }}>THE CLIMB</span>
-            </h1>
-            <h4>We help founders build their dreams and scale for success</h4>
+          <div className={styles.container}>
+            <div className={styles.textBox}>
+              <h1>
+                <span style={{ whiteSpace: "nowrap" }}>ACCELERATING</span>{" "}
+                <br />
+                <span style={{ whiteSpace: "nowrap" }}>THE CLIMB</span>
+              </h1>
+              <h4>We help founders build their dreams and scale for success</h4>
+            </div>
             <div className={styles.buttons}>
               <a href="#contact">
                 <button className={`${styles.btnFull} ${styles.btn}`}>
