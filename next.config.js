a67df9507575cc659289ca6cdfca2dd7cbe802d4
Marcel Barbulescu <marcelbarbulescu@gmail.com>
2022-08-29 16:01:54 -0400

    Upgrade all dependencies to the latest version and remove '@zeit/next-css'

--- next.config.js
+++ next.config.js
@@ -1,29 +1,25 @@
 // next.config.js
 const withPlugins = require("next-compose-plugins");
-const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
 const withTM = require("next-transpile-modules");
 require("dotenv").config();
 
 module.exports = withPlugins(
   [
-    [withImages],
+    [
+      withImages,
+      {
+        images: {
+          disableStaticImages: true
+        }
+      }
+    ],
     [
       withTM,
       {
         transpileModules: ["lodash-es"]
       }
     ],
-    [
-      withCSS,
-      {
-        cssModules: true,
-        cssLoaderOptions: {
-          importLoaders: 1,
-          localIdentName: "[local]___[hash:base64:5]"
-        }
-      }
-    ]
   ],
   {
     webpack: config => {
