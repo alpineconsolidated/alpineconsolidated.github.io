ace64ca9cb95df84e142de1caba8f8a961d4a607
Marcel Barbulescu <marcelbarbulescu@gmail.com>
2022-11-15 08:45:48 -0500

    Apply new-link codemod

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,99 +24,97 @@ class Navbar extends React.Component {
   };
 
   render() {
-    return (
-      <>
-        <nav className={`${styles.navbar}`}>
-          <div className={`${styles.container} box-wide`}>
-            <div className={styles.logo}>
-              <img src={this.props.logo}></img>
-            </div>
-            <div onClick={this.onMenuClick} className={styles.burgerMenu}>
-              <div
-                className={`${styles.menuLine} ${
-                  this.state.menuVisible ? styles.closing : ""
-                }`}
-              ></div>
-              <div
-                className={`${styles.menuLine} ${
-                  this.state.menuVisible ? styles.closing : ""
-                }`}
-              ></div>
-              <div
-                className={`${styles.menuLine} ${
-                  this.state.menuVisible ? styles.closing : ""
-                }`}
-              ></div>
-            </div>
-            <ul className={styles.menu}>
-              <li className={styles.menuItem}>
-                <Link href="/">
-                  <a
-                    className={
-                      this.props.router.route === "/" ? styles.active : ""
-                    }
-                  >
-                    Alpine Consolidated
-                  </a>
-                </Link>
-              </li>
+    return <>
+      <nav className={`${styles.navbar}`}>
+        <div className={`${styles.container} box-wide`}>
+          <div className={styles.logo}>
+            <img src={this.props.logo}></img>
+          </div>
+          <div onClick={this.onMenuClick} className={styles.burgerMenu}>
+            <div
+              className={`${styles.menuLine} ${
+                this.state.menuVisible ? styles.closing : ""
+              }`}
+            ></div>
+            <div
+              className={`${styles.menuLine} ${
+                this.state.menuVisible ? styles.closing : ""
+              }`}
+            ></div>
+            <div
+              className={`${styles.menuLine} ${
+                this.state.menuVisible ? styles.closing : ""
+              }`}
+            ></div>
+          </div>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>
+              <Link
+                href="/"
+                className={
+                  this.props.router.route === "/" ? styles.active : ""
+                }>
+                
+                  Alpine Consolidated
+                
+              </Link>
+            </li>
 {/*
-              <li className={styles.menuItem}>
-                <Link href="/TEC">
-                  <a
-                    className={
-                      this.props.router.route === "/TEC" ? styles.active : ""
-                    }
-                  >
-                    AlpineTEC Ventures
-                  </a>
-                </Link>
-              </li>
+            <li className={styles.menuItem}>
+              <Link href="/TEC">
+                <a
+                  className={
+                    this.props.router.route === "/TEC" ? styles.active : ""
+                  }
+                >
+                  AlpineTEC Ventures
+                </a>
+              </Link>
+            </li>
 */}
-              <li className={styles.menuItem}>
-                <a href="#contact">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        {this.state.menuVisible && (
-          <div
-            className={`${this.state.closing ? styles.closingMenu : ""} ${
-              styles.mobileMenu
-            }`}
-          >
-            <ul>
-              <li className={styles.menuItem}>
-                <Link href="/">
-                  <a
-                    className={
-                      this.props.router.route === "/" ? styles.active : ""
-                    }
-                  >
-                    Alpine Consolidated
-                  </a>
-                </Link>
-              </li>
-              <li className={styles.menuItem}>
-                <Link href="/TEC">
-                  <a
-                    className={
-                      this.props.router.route === "/TEC" ? styles.active : ""
-                    }
-                  >
-                    AlpineTEC Ventures
-                  </a>
-                </Link>
-              </li>
-              <li onClick={this.onMenuClick} className={styles.menuItem}>
-                <a href="#contact">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        )}
-        <div className={styles.spacer}></div>
-      </>
-    );
+            <li className={styles.menuItem}>
+              <a href="#contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {this.state.menuVisible && (
+        <div
+          className={`${this.state.closing ? styles.closingMenu : ""} ${
+            styles.mobileMenu
+          }`}
+        >
+          <ul>
+            <li className={styles.menuItem}>
+              <Link
+                href="/"
+                className={
+                  this.props.router.route === "/" ? styles.active : ""
+                }>
+                
+                  Alpine Consolidated
+                
+              </Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link
+                href="/TEC"
+                className={
+                  this.props.router.route === "/TEC" ? styles.active : ""
+                }>
+                
+                  AlpineTEC Ventures
+                
+              </Link>
+            </li>
+            <li onClick={this.onMenuClick} className={styles.menuItem}>
+              <a href="#contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      )}
+      <div className={styles.spacer}></div>
+    </>;
   }
 }
 export default withRouter(Navbar);
