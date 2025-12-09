8bef35b82b62e566413c21762232d898bfae268d
Marcel Barbulescu <marcelbarbulescu@gmail.com>
2025-12-08 23:05:34 -0500

    Remove SendGrid vestiges

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-require("dotenv").config();
 
 const nextConfig = {
-  env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  },
-  output: 'export',
   images: {
     unoptimized: true,
   },
+  output: 'export',
   transpilePackages: ['lodash-es'],
 };
 
