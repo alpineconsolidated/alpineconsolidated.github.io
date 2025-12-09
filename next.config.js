bfbd0da0176a3809ba9371e78f832589a1eb280e
Marcel Barbulescu <marcelbarbulescu@gmail.com>
2025-12-08 21:13:53 -0500

    Upgrade to Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,38 +1,12 @@
-// next.config.js
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withTM = require("next-transpile-modules");
+/** @type {import('next').NextConfig} */
 require("dotenv").config();
 
-module.exports = withPlugins(
-  [
-    [
-      withImages,
-      {
-        images: {
-          disableStaticImages: true
-        }
-      }
-    ],
-    [
-      withTM,
-      {
-        transpileModules: ["lodash-es"]
-      }
-    ],
-  ],
-  {
-    output: 'export',
-    webpack: config => {
-      return config;
-    }
+const nextConfig = {
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   },
-  {
-    env: {
-      SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
-    }
-  },
-  {
-    target: "serverless"
-  }
-);
+  output: 'export',
+  transpilePackages: ['lodash-es'],
+};
+
+module.exports = nextConfig;
